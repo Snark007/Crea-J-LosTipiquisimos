d42d78941e8bc37c1c5bcd8b8366ada71d127bf5
Ferchofloo <105886720+Ferchofloo@users.noreply.github.com>
2022-07-18 15:46:09 -0600

    e

--- php/index.php
+++ php/index.php
@@ -1,14 +1,6 @@
 <?php
-
-$server = "localhost";
-$user = "root";
-$pass = "";
-$db = "tipiquisimos";
-
-$conexion = mysqli_connect($server,$user,$pas,$db);
-
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-  }
-  
+   $conexion = mysqli_connect("localhost" , "root" , "" , "tipiquisimos" , "3306")
+   if ($conexion) {
+     echo "conexion exitosa"
+   }
+?> 
