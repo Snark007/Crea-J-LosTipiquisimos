4f7ed7af32270d4e590b44c382cbafab57c4b146
Ferchofloo <105886720+Ferchofloo@users.noreply.github.com>
2022-08-07 22:14:21 -0600

    icon

--- Html/hader.php
+++ Html/hader.php
@@ -1,8 +1,6 @@
 <?php
-    session_start();
-
+session_start();
 ?>
-
 <header>
     <div class="wrapper-menu">
         <div class="btn-translate-hide wrapper-caja ">
@@ -36,31 +34,31 @@
                 <div class="more-options-recetas">
                     <p><a href="../COMIDAS/base-navidena.php"><span data-nav="6">Navideñas</span> </a></p>
                     <p><a href="../COMIDAS/base-semanasanta.php"><span data-nav="7">Semana santa</span></a></p>
-                    <p><a href="../COMIDAS/base-pupusas.php"><span  data-nav="8">pupusas</span></a></p>
-                    <p><a href="../COMIDAS/base-postres.php"><span  data-nav="9">postres</span></a></p>
-                    <p><a href="../COMIDAS/base-sopas.php"><span  data-nav="10">Tamales</span></a></p>
-                    <p><a href="../COMIDAS/base-sopas.php"><span  data-nav="11">Sopas </span></a></p>
+                    <p><a href="../COMIDAS/base-pupusas.php"><span data-nav="8">pupusas</span></a></p>
+                    <p><a href="../COMIDAS/base-postres.php"><span data-nav="9">postres</span></a></p>
+                    <p><a href="../COMIDAS/base-sopas.php"><span data-nav="10">Tamales</span></a></p>
+                    <p><a href="../COMIDAS/base-sopas.php"><span data-nav="11">Sopas </span></a></p>
                     <p><a href="../COMIDAS/base-tipica.php"><span data-nav="12">Recetas tipicas</span></a></p>
                     <p><a href="../COMIDAS/base-atoles.php"><span data-nav="13">Atoles</span></a></p>
                 </div>
 
             </div>
             </a>
-            <a href="../Html/subir.php"  data-nav="14">¿Quieres subir tu receta?</a>
-            <a href="../COMIDAS/Comunidad.php"  data-nav="15">Comunidad</a>
+            <a href="../Html/subir.php" data-nav="14">¿Quieres subir tu receta?</a>
+            <a href="../COMIDAS/Comunidad.php" data-nav="15">Comunidad</a>
         </nav>
 
-        
+
         <?php if (!isset($_SESSION['aña'])) { ?>
-               <a href="../Html/Iniciarsecion.php" class="hide-login"><button class="but"  data-nav="16">Iniciar sesión</button></a>             
-        <?php } else {  ?>  
-            <p class="main-text"><a href="" data-nav="5"><img src="../icons/icons8-user-24.png" alt=""></a></p>
+            <a href="../Html/Iniciarsecion.php" class="hide-login"><button class="but" data-nav="16">Iniciar sesión</button></a>
+        <?php } else {  ?>
+            <p class="main-text"><a href="" data-nav="5"><img src="../icons/icon-iniciar.png"></a></p>
 
             <div class="more-options-recetas-i">
-            <p><a href="../Html/close.php">cerrar sesion</a></p>
+                <p><a href="../Html/close.php">cerrar sesion</a></p>
             </div>
 
-        <?php } ?>               
-        
+        <?php } ?>
+
     </div>
 </header>
