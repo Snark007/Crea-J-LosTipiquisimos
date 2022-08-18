6f35efdee02a36960be512a71600c937137a37c2
Ferchofloo <105886720+Ferchofloo@users.noreply.github.com>
2022-08-18 16:38:04 -0600

    cositas

---
+++ COMIDAS/plantilla.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/stilo.css">
+    <link rel="icon" href="../Img/cocina.png">
+    <link rel="stylesheet" href="../Css/translate.css">
+    <link rel="stylesheet" href="../Css/menuD.css">
+    <script defer src="../Js/menu.js"></script>
+    <script defer src="../Js/translate.js"></script>
+    <title>Arroz con leche</title>
+</head>
+<body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/stilo.css">
+    <link rel="stylesheet" href="../Css/estructura.css">
+    <link rel="stylesheet" href="../Css/translate.css">
+    <link rel="stylesheet" href="../Css/menuD.css">
+    <script defer src="../Js/menu.js"></script>
+    <script defer src="../Js/translate.js"></script>
+    <title>Atol de Piñeula</title>
+</head>
+
+<body>
+   <!--MENU-->
+   <?php
+    include "../Html/hader.php";
+    ?>
+    <!--BASE-->
+    <section>
+        <?php 
+        $conexion = mysqli_connect('localhost', 'root', '', 'db_tipiquisimos');
+        ?>
+        <div class="recetas">
+            <img src="../Comida/arroz-con-leche.jpg" alt="">
+            <div class="recetas-letras">
+                <h1>ARROZ CON LECHE</h1>
+                <p>
+                    El arroz con leche es un postre casero y cremoso elaborado 
+                    principalmente con arroz y leche, como su nombre indica. 
+                    Su receta original es disfrutada en prácticamente cualquier lugar 
+                    del mundo, lo que también ha dado lugar a algunas variedades sobre el 
+                    postre tradicional. Aunque normalmente se consume de último en el menú y para
+                     cerrar la velada , en algunos países europeos se sirve como desayuno, ya que es 
+                     muy similar a las gachas de arroz.
+                </p>
+            </div>
+        </div>
+        <div class="linea">
+            <hr>
+        </div>
+        <div class="ingredientes">
+            <div class="ingredientes2">
+                <h1>Ingredientes:</h1>
+            <p>
+                <ul type="square">
+                    <li> 4 tazas de leche</li>
+            <li> 6 tazas de agua</li>
+            <li> 1 taza de azúcar</li>
+            <li> 1/3 taza de arroz de grano largo</li>
+            <li> 4 canela en rama</li>
+            <li> 2 cucharadas de mantequilla</li>
+            <li> 1 pizca de sal</li>
+            <li> 1/8 cucharadita de nuez moscada en polvo</li>
+            <li> ¼ taza de pasas</li>
+                </ul>
+            </p>
+            </div>
+            <div class="ingredientes-pasos">
+                <h1>Preparación:</h1>
+                <ol>
+                    <li>Preparación previa: Empapa el arroz en una mezcla de 1 taza de leche y 1 de agua y pon el refrigerador por unas horas antes.
+
+                    </li><br>
+    
+                <li>Esto te ahorrará tiempo de cocción.
+
+                </li><br>
+    
+                <li>Pon las 4 tazas restantes de agua en un caldero y pon al fuego, agrega la canela y la sal.
+
+                </li><br>
+    
+                <li>Lleva ebullición y después agrega el arroz (con el líquido en que estaba remojado).
+
+                </li><br>
+
+                <li>Revuelve regularmente para evitar que se adhiera al fondo.
+
+                </li><br>
+
+                <li>Ajusta el agua cuando sea necesario y cuando el arroz esté muy blando agrega la leche y la mitad del azúcar.
+
+                </li><br>
+
+                <li>Cuando empiece a hervir agrega las pasas y el azúcar restante.
+
+                </li><br>
+
+                <li>Cuando tenga una consistencia muy espesa, casi sólida, retira del fuego y vierte inmediatamente en tazones pequeños o copas para helado.
+
+                </li><br>
+
+                <li>Espolvorea con nuez moscada moscada molida y deja refrescar.
+
+                </li><br>
+
+                <li>Pon en el refrigerador por 30 minutos antes de servir.</li>
+            </ol>        
+            </div>
+        </div>
+    </section>
+      <!--::::Pie de Pagina::::::-->
+      <footer>
+        <div class="contar">
+            <h3 class="log">Los Tipiquisimos</h3>
+            <p>&copy; Los Tipiquisimos 2022-Derechos reservados</p>
+        </div>
+        <div class="container3">
+            <a href="https://www.facebook.com/Los-Tipiquisimos-110215061699961/?ref=page_internal"><img
+                    src="../Img/facebook.png" alt="" class="Photographs"></a><br>
+            <a href="https://www.instagram.com/los_tipiquisimos.sv/"><img src="../Img/instagram.png" alt=""
+                    class="Photographs"></a><br>
+            <a href="https://twitter.com/Tipiquisimos503"><img src="../Img/twitter.png" alt=""
+                    class="Photographs"></a><br>
+        </div>
+    </footer>
+</body>
+</html>
--- Html/v-subir.php
+++ Html/v-subir.php
@@ -6,27 +6,34 @@ if (!$conexion) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-insertar($conexion);
 
-function insertar($conexion)
-{
-    $nombre_r = $_POST['nombre'];
+
+if ($_POST) {
     $descripcion_r = $_POST['desc'];
     $ingredientes_r = $_POST['ingre'];
     $preparacion_r = $_POST['proce'];
-    $img = $_POST['img-receta'];
+    $img = $_FILES['img-receta']['name'];
+    $ruta = "../IMG-comunidad/".$_FILES['img-receta']['name'];
+    //$imgT = $_FILES['img-receta']['tmp_name'];
+    $nombre_r = $_POST['nombre'];
 
+    //print_r($_FILES['img-receta']);
+    //Funcion para guardar imagenes - move_uploaded_file pide dos argumento
+    //1-Nombre temporal 2-Nombre con el que se va a guardar
+    move_uploaded_file($_FILES['img-receta']['tmp_name'],"../IMG-comunidad/".$_FILES['img-receta']['name']);
 
-
-    $sql = "INSERT INTO `subir` (`id-subir`, `nombre-receta`, `descripcion`, `ingredientes`, `pasos`, `imagen`, `creador`) VALUES (NULL, '$nombre_r', '$descripcion_r', '$ingredientes_r', '$preparacion_r', '$img')";
-    if (mysqli_query($conexion, $sql)) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
-    }
-
-    mysqli_close($conexion);
-    header("location:subir.php");
+    $sql = "INSERT INTO `subir` (`id-subir`, `nombre-receta`, `descripcion`, `ingredientes`, `pasos`, `imagen`)
+     VALUES (NULL, '$nombre_r', '$descripcion_r', '$ingredientes_r', '$preparacion_r', '$ruta');";
 }
+
+if (mysqli_query($conexion, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
+}
+
+mysqli_close($conexion);
+header("location:subir.php");
+
 
 ?>
