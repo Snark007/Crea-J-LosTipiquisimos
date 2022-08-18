b07a9bc2710e0ef74929a1c83cd49e55c0453af0
Papapio123 <eduardopenate3@gmail.com>
2022-08-18 17:32:31 -0600

    inzano
    Arregle la traduccion de el tamal de gallina y realize la traduccion de las recetas de navidad solo hace falta 1

--- COMIDAS/panes-pollo.php
+++ COMIDAS/panes-pollo.php
@@ -9,21 +9,8 @@
     <link rel="stylesheet" href="../Css/translate.css">
     <link rel="stylesheet" href="../Css/menuD.css">
     <script defer src="../Js/menu.js"></script>
-    <script defer src="../Js/translate.js"></script>
+    <script defer src="../Js/translate_recetas-panpollo.js"></script>
     <title>Panes con pollo</title>
-</head>
-<body>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Css/estructura.css">
-    <link rel="stylesheet" href="../Css/translate.css">
-    <link rel="stylesheet" href="../Css/menuD.css">
-    <link rel="stylesheet" href="../css/stilo.css">
-    <script defer src="../Js/menu.js"></script>
-    <script defer src="../Js/translate.js"></script>
-    <title>Atol de Piñeula</title>
 </head>
 
 <body>
@@ -36,13 +23,13 @@
         <div class="recetas">
             <img src="../Comida/pan-pollo.jpg" alt="">
             <div class="recetas-letras">
-                <h1>PANES CON POLLO</h1>
+                <h1><span data-panpollo="1">PANES CON POLLO</span></h1>
                 <p>
-
+                <span data-panpollo="2">
                     Muy ricos aperitivos de El Salvador que podemos degustar de forma tradicional en fiestas patrióticas o 
                     en las fiestas decembrinas. La alternativa ideal para conocer la gastronomía de la tierra centroamericana, 
                     que envuelve con su deliciosa salsa de pollo, sazón salvadoreña
-
+                    </span>
                 </p>
             </div>
         </div>
@@ -51,30 +38,30 @@
         </div>
         <div class="ingredientes">
             <div class="ingredientes2">
-                <h1>Ingredientes:</h1>
+                <h1><span data-panpollo="3">Ingredientes:</span></h1>
             <p>
                 <ul type="square">
-                    <li>30 jocotes</li>
-                    <li> 2 litros de agua</li>
-                    <li> 1 dulce de atado (panela) ó 1 lb de azúcar</li>
-                    <li> Canela al gusto</li>
-                    <li> 2/3 taza de ceniza</li>
+                    <li><span data-panpollo="4">30 jocotes</span></li>
+                    <li><span data-panpollo="5"> 2 litros de agua</span></li>
+                    <li><span data-panpollo="6"> 1 dulce de atado (panela) ó 1 lb de azúcar</span></li>
+                    <li><span data-panpollo="7"> Canela al gusto</span></li>
+                    <li><span data-panpollo="8"> 2/3 taza de ceniza</span></li>
                
                 </ul>
             </p>
             </div>
             <div class="ingredientes-pasos">
-                <h1>Preparación:</h1>
+                <h1><span data-panpollo="9">Preparación:</span></h1>
                 <ol>
-                    <li>Prepare una mezcla de agua para agregar la ceniza. Posteriormente hervirla. Cuando el agua rompa el hervor agregue los jocotes.</li><br>
+                    <li><span data-panpollo="10">Prepare una mezcla de agua para agregar la ceniza. Posteriormente hervirla. Cuando el agua rompa el hervor agregue los jocotes.</span></li><br>
     
-                    <li>Una vez vea que los jocotes se han agrietado sáquelos del agua y quíteles la cáscara con la mano. Cuando los haya pelado corte en cruz sin quitar la semilla y resérvelos para próximo paso.</li><br>
+                    <li><span data-panpollo="11">Una vez vea que los jocotes se han agrietado sáquelos del agua y quíteles la cáscara con la mano. Cuando los haya pelado corte en cruz sin quitar la semilla y resérvelos para próximo paso.</span></li><br>
         
-                    <li>En otra olla ponga a hervir a fuego lento el agua con el dulce de atado y la canela. Agregue los jocotes ya pelados y déjelos cocinar durante una hora.</li><br>
+                    <li><span data-panpollo="12">En otra olla ponga a hervir a fuego lento el agua con el dulce de atado y la canela. Agregue los jocotes ya pelados y déjelos cocinar durante una hora.</span></li><br>
         
-                    <li>Revise si la miel ya está conservada. Sino déjela hasta conservar. La ceniza permite pelar mejor los jocotes, que éstos no pierdan su textura ni su tamaño.</li><br>
+                    <li><span data-panpollo="13">Revise si la miel ya está conservada. Sino déjela hasta conservar. La ceniza permite pelar mejor los jocotes, que éstos no pierdan su textura ni su tamaño.</span></li><br>
     
-                    <li>Recomendación: Seleccionar jocotes de tipo ácido (en Semana Santa). También puede hacerse del tipo dulce de invierno (de junio hasta agosto). No se recomienda hacerlo del de corona (de agosto a septiembre).</li>
+                    <li><span data-panpollo="14">Recomendación: Seleccionar jocotes de tipo ácido (en Semana Santa). También puede hacerse del tipo dulce de invierno (de junio hasta agosto). No se recomienda hacerlo del de corona (de agosto a septiembre).</span></li>
             </ol>        
             </div>
         </div>
@@ -83,7 +70,7 @@
       <footer>
         <div class="contar">
             <h3 class="log">Los Tipiquisimos</h3>
-            <p>&copy; Los Tipiquisimos 2022-Derechos reservados</p>
+            <p>&copy;<span data-panpollo="15"> Los Tipiquisimos 2022-Derechos reservados</span></p>
         </div>
         <div class="container3">
             <a href="https://www.facebook.com/Los-Tipiquisimos-110215061699961/?ref=page_internal"><img
---
+++ Js/translate_recetas-panpollo.js
@@ -0,0 +1,131 @@
+// let español, english;
+let panpolloEspañol = `
+PANES CON POLLO
+Muy ricos aperitivos de El Salvador que podemos degustar de forma tradicional en fiestas patrióticas o en las fiestas decembrinas. La alternativa ideal para conocer la gastronomía de la tierra centroamericana, que envuelve con su deliciosa salsa de pollo, sazón salvadoreña
+Ingredientes:
+30 jocotes
+2 litros de agua
+1 dulce de atado (panela) ó 1 lb de azúcar
+Canela al gusto
+2/3 taza de ceniza
+Preparación:
+Prepare una mezcla de agua para agregar la ceniza. Posteriormente hervirla. Cuando el agua rompa el hervor agregue los jocotes.
+Una vez vea que los jocotes se han agrietado sáquelos del agua y quíteles la cáscara con la mano. Cuando los haya pelado corte en cruz sin quitar la semilla y resérvelos para próximo paso.
+En otra olla ponga a hervir a fuego lento el agua con el dulce de atado y la canela. Agregue los jocotes ya pelados y déjelos cocinar durante una hora.
+Revise si la miel ya está conservada. Sino déjela hasta conservar. La ceniza permite pelar mejor los jocotes, que éstos no pierdan su textura ni su tamaño.
+Recomendación: Seleccionar jocotes de tipo ácido (en Semana Santa). También puede hacerse del tipo dulce de invierno (de junio hasta agosto). No se recomienda hacerlo del de corona (de agosto a septiembre).
+Los Tipiquisimos 2022-Derechos reservados
+`
+let panpolloIngles = `
+BREAD WITH CHIKEN
+Very delicious appetizers from El Salvador that we can taste in a traditional way at patriotic parties or in the December holidays. The ideal alternative to know the gastronomy of the Central American land, that wraps with its delicious chicken sauce, Salvadoran seasoning
+Ingredients:
+30 jocotes
+2 liters of water
+1 tied sweet (panela) or 1 lb of sugar
+Cinnamon to taste
+2/3 cup ash
+Preparation:
+Prepare a mixture of water to add the ash. Then boil it. When the water breaks the boil add the jocotes.
+Once you see that the jocotes have cracked, take them out of the water and remove the shell by hand. When you have peeled them, cut them crosswise without removing the seed and reserve them for the next step.
+In another pot, simmer the water with the sweet tied and cinnamon. Add the already peeled jocotes and let them cook for an hour.
+Check if the honey is already preserved. Otherwise, leave it until you keep it. The ash allows the jocotes to be peeled better, so that they do not lose their texture or their size.
+Recommendation: Select acid-type jocotes (at Easter). It can also be made of the sweet winter type (June through August). It is not recommended to do it from the corona (from August to September).
+Los Tipiquisimos 2022-Rights reserved
+`
+let navEspañol = `
+Elegir idioma
+España
+Ingles
+¡Sobre nosotros!
+Recetas
+Navideñas
+Semana santa
+pupusas
+postres
+Tamales
+Sopas
+Recetas tipicas
+Atoles
+¿Quieres subir tu receta?
+Comunidad
+Inciar sesion
+`
+let navIngles = `
+Choose a language
+Spanish
+English
+¡About us!
+Recipes
+Christmas
+Easter
+pupusas
+desserts
+Tamales
+Soups
+Typical recipes
+Atoles
+¿Want to upload your recipes?
+Community
+Log in
+`
+panpolloIngles = panpolloIngles.split("\n");
+panpolloEspañol = panpolloEspañol.split("\n");
+
+navIngles = navIngles.split("\n");
+navEspañol = navEspañol.split("\n");
+
+lang = sessionStorage.getItem("lang");
+console.log(lang)
+
+if(!lang){
+    lang = sessionStorage.setItem("lang", "es");
+}
+
+
+
+function  traducir(element) {
+    console.log(element)
+    if(element.id == "idioma-ingles"){   
+        
+        navIngles.forEach((element, i) => {
+            if(!element) return
+             
+            if (document.querySelector("[data-nav='"+i+"']")) {
+                document.querySelector("[data-nav='"+i+"']").innerHTML = element;
+            }
+        });
+        panpolloIngles.forEach((element, i) => {
+            if(!element) return
+             
+            document.querySelector("[data-panpollo='"+i+"']").innerHTML = element;
+        });
+
+       sessionStorage.setItem("lang", "en");
+       lang = sessionStorage.getItem("lang");
+
+
+    }
+    else if(element.id == "idioma-español"){
+        navEspañol.forEach((element, i) => {
+            if(!element) return
+            if (document.querySelector("[data-nav='"+i+"']")) {
+                
+                document.querySelector("[data-nav='"+i+"']").innerHTML = element;
+            }
+        });
+        panpolloEspañol.forEach((element, i) => {
+            if(!element) return
+            document.querySelector("[data-panpollo='"+i+"']").innerHTML = element;
+        });
+
+       sessionStorage.setItem("lang", "es");
+       lang = sessionStorage.getItem("lang");
+    }
+    console.log(lang)
+}
+
+document.querySelector(".wrapper-caja").addEventListener("click", ()=>{
+    document.querySelector(".options").classList.toggle("mostrar");
+    document.querySelector(".caja__arrow").classList.toggle("changeOrientation");
+})
