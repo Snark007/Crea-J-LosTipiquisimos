a8eb47aacd09634ef16b9b3ed616ad43ca96d4fc
MichaelLumi <105886911+Snark007@users.noreply.github.com>
2022-08-22 10:08:51 -0600

    perfil de usuario terminado
    ya esta el perfil

--- Html/usuario.php
+++ Html/usuario.php
@@ -3,26 +3,42 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Apock web design</title>
+    <title>Perfil</title>
+    <link rel="icon" href="../Img/cocina.png">
+    <link rel="stylesheet" href="../Css/estructura.css">
+    <link rel="stylesheet" href="../Css/translate.css">
+    <link rel="stylesheet" href="../Css/menuD.css">
     <link rel="stylesheet" href="../css/prueba.css">
+    <script defer src="../Js/menu.js"></script>
+    <script defer src="../Js/translate_index.js"></script>
 </head>
 
 <body>
-
+    <!--MENU-->
+    <?php
+    include "hader.php";
+    ?>
+    <!--BASE-->
     <section class="seccion-perfil-usuario">
         <div class="perfil-usuario-header">
             <div class="perfil-usuario-portada">
                 <div class="perfil-usuario-avatar">
-                    <img src="../Img/messi.png" alt="img-avatar">
-                        <i class="far fa-image"></i>
+                    <img src="../Img/chef.png" alt="img-avatar">
+
+                    <i class="far fa-image"></i>
                     </button>
                 </div>
+                <button type="button" class="boton-portada">
+                    <i class="far fa-image"></i> Cambiar foto de perfil
+                </button>
             </div>
         </div>
         <div class="perfil-usuario-body">
             <div class="perfil-usuario-bio">
-                <h3 class="titulo">Lionel Andrés Messi Cuccittini</h3>
+                <h3 class="titulo">Gordon James Ramsay</h3>
+
             </div>
             <div class="perfil-usuario-footer">
                 <ul class="lista-datos">
@@ -31,15 +47,15 @@
                     <li><i class="icono fas fa-briefcase"></i> Contraseña</li>
                 </ul>
                 <ul class="lista-datos">
-                    <li><i class="icono fas fa-map-marker-alt"></i> Messi</li>
-                    <li><i class="icono fas fa-calendar-alt"></i> FcBarcelona@gmail.com</li>
+                    <li><i class="icono fas fa-map-marker-alt"></i> Ramsay</li>
+                    <li><i class="icono fas fa-calendar-alt"></i> Gordon@gordonramsay.com</li>
                     <li><i class="icono fas fa-user-check"></i> *********</li>
                 </ul>
             </div>
         </div>
     </section>
- 
 
+    </div>
 </body>
 
 </html>
