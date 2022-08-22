a495434759953d8a64ed47897cf95f69ab5dd417
Aletorres414 <eduardopenate3@gmail.com>
2022-08-22 13:24:43 -0600

    b
    aña

--- Js/translate_recetas-pupupusaq.js
+++ Js/translate_recetas-pupupusaq.js
@@ -1,41 +1,36 @@
 // let español, english;
-let pupusafqEspañol = `
+let pupusaqEspañol = `
 PUPUSA DE QUESO
-Las pupusas son un platillo tradicional de El Salvador, existen de diferentes rellenos, en este caso te dejamos la receta de pupusas con frijos y queso, sin embgardo puedes hacerla solo de queso o frijoles, con chicharrones, etc. Es importante que tengas a tu disposición 1 plancha para poder realizarlas y que queden bien doraditas.
+Las pupusas son el plato típico de El Salvador y muy popular en Honduras. Esta delicia muestra la versatilidad del maíz como producto básico en la cocina de los países mesoamericanos. Se pueden comparar con las gorditas de maíz mexicanas o las arepas rellenas, pero los rellenos y la cocción son diferentes. La principal diferencia es que las pupusas se rellenan antes de cocerse y las gorditas y arepas se rellenan después de cocidas. Esto da a las pupusas una rica combinación del sabor del maíz con el relleno con el que se cocina, comúnmente queso, frijoles o chicharrones.
 Ingredientes:
 Medio kilo de harina de maíz o de arroz
 Agua tibia
 Quesillo
 Sal
 Aceite
-Chicharron molido
-Frijoles molidos fritos
 Preparación:
 El primer paso a realizar, es la elaboración de la masa, para lo que necesitaremos una taza, donde servirás Medio kilo de harina de maíz o arroz ( o la cantidad que gustes, al final nunca es mucha) y agrega agua tibia. Para unir ambos ingredientes, debes usar tus manos, amasando constantemente para que no queden grumos.
 Si al amasar notamos que la masa está algo dura, agregaremos un poco más de agua junto con algo de sal para dar sabor. Finalmente, nuestra masa no debe quedar ni tan aguada ni tan dura, de modo que puedas hacer una pelotita con ella y no se desmorone.
-Debes tener un envase con agua y aceite, el cual te ayudará a dar forma a las pupusas, de manera que la masa no se pegue a tus manos. Ahora, con las manos humedecidas, debes tomar una pelotita de masa y con ambas manos la vas aplanando hasta que quede como una tortilla.
-Mientras das forma a la masa, debes ir humedeciendo tus manos en el agua. Si se te hace complicado, puedes utilizar tus dedos para hacer en el centro una pequeña hendidura, donde colocarás el relleno. Tomando un poco de chicharrón y frijoles revueltos previamente con el quesillo.
+tener un envase con agua y aceite, el cual te ayudará a dar forma a las pupusas, de manera que la masa no se pegue a tus manos. Ahora, con las manos humedecidas, debes tomar una pelotita de masa y con ambas manos la vas aplanando hasta que quede como una tortilla.
+Mientras das forma a la masa, debes ir humedeciendo tus manos en el agua. Si se te hace complicado, puedes utilizar tus dedos para hacer en el centro una pequeña hendidura, donde colocarás el relleno. Tomando un poco de quesillo.
 Una vez que has colocado el relleno, se procede a envolver, dando forma de esférica. Finalmente, vuelves a aplanar y se coloca la pupusa en un comal o plancha bien caliente. Después de un rato, debes voltear con una espátula, de forma que no se pasen de cocción. Eso sí, cada vez que des vuelta a las pupusas debes mojar la espátula en el agua con aceite, así será más fácil. Con esto, ya estás listo para degustar unas ricas pupusas totalmente salvadoreñas.
-Los Tipiquisimos 2022-Derechos reservados
 `
-let pupusafqIngles = `
-PUPUSA DE QUESO
-Pupusas are a traditional dish from El Salvador, there are different fillings, in this case we leave you the recipe for pupusas with beans and cheese, however you can make it only with cheese or beans, with pork rinds, etc. It is important that you have at your disposal 1 iron to be able to make them and that they are well browned.
+let pupusaqIngles = `
+CHEESE PUPUSA
+Pupusas are the typical dish of El Salvador and very popular in Honduras. This delicacy shows the versatility of corn as a basic product in the cuisine of Mesoamerican countries. They can be compared to Mexican corn gorditas or stuffed arepas, but the fillings and cooking are different. The main difference is that pupusas are stuffed before cooking and gorditas and arepas are stuffed after cooking. This gives pupusas a rich combination of the flavor of the corn with the filling with which it is cooked, commonly cheese, beans or chicharrones.
 Ingredients:
-Half a kilo of corn or rice flour
+Half a kilo of corn or rice flour.
 Warm water
-quesillo
+Cheese
 Salt
 Oil
-ground pork rinds
-fried ground beans
 Preparation:
-The first step to carry out is the preparation of the dough, for which we will need a cup, where you will serve half a kilo of corn or rice flour (or the amount you like, in the end it is never too much) and add warm water. To unite both ingredients, you must use your hands, constantly kneading so that there are no lumps.
-If when kneading we notice that the dough is somewhat hard, we will add a little more water along with some salt to give flavor. Finally, our dough should not be so watery or so hard, so that you can make a ball with it and it does not fall apart.
-You must have a container with water and oil, which will help you shape the pupusas, so that the dough does not stick to your hands. Now, with moistened hands, you must take a small ball of dough and with both hands you flatten it until it is like a tortilla.
-While you shape the dough, you must moisten your hands in the water. If you find it difficult, you can use your fingers to make a small indentation in the center, where you will place the filling. Taking a little pork rind and beans previously mixed with the quesillo.
-Once you have placed the filling, proceed to wrap it, giving it a spherical shape. Finally, flatten again and place the pupusa on a very hot griddle or griddle. After a while, you must turn with a spatula, so that they do not overcook. Of course, every time you turn the pupusas you must dip the spatula in the water with oil, that way it will be easier. With this, you are ready to taste some delicious totally Salvadoran pupusas.
-Los Tipiquisimos 2022-Rights reserved
+The first step to do, is the preparation of the dough, for which we will need a cup, where you will serve Half a kilo of corn or rice flour (or the amount you like, in the end it is never much) and add warm water. To unite both ingredients, you must use your hands, kneading constantly so that there are no lumps.
+If when kneading we notice that the dough is a little hard, we will add a little more water along with some salt to give flavor. Finally, our dough should be neither too watery nor too hard, so that you can make a little ball with it and it will not fall apart.
+Have a container with water and oil, which will help you shape the pupusas, so that the dough does not stick to your hands. Now, with wet hands, take a small ball of dough and with both hands flatten it until it looks like a tortilla.
+While you shape the dough, you should wet your hands in the water. If you find it difficult, you can use your fingers to make a small indentation in the center, where you will place the filling. Taking a little bit of quesillo.
+Once you have placed the filling, proceed to wrap, giving it a spherical shape. Finally, flatten again and place the pupusa on a hot griddle or griddle. After a while, you should turn them over with a spatula, so that they do not overcook. Of course, every time you turn the pupusas you should dip the spatula in the oiled water, so it will be easier. With this, you are ready to taste some delicious and totally Salvadoran pupusas.
+Translated with www.DeepL.com/Translator (free version)
 `
 let navEspañol = `
 Elegir idioma
@@ -73,8 +68,8 @@ Atoles
 Community
 Log in
 `
-pupusafqIngles = pupusafqIngles.split("\n");
-pupusafqEspañol = pupusafqEspañol.split("\n");
+pupusaqIngles = pupusaqIngles.split("\n");
+pupusaqEspañol = pupusaqEspañol.split("\n");
 
 navIngles = navIngles.split("\n");
 navEspañol = navEspañol.split("\n");
@@ -99,10 +94,10 @@ function  traducir(element) {
                 document.querySelector("[data-nav='"+i+"']").innerHTML = element;
             }
         });
-        pupusafqIngles.forEach((element, i) => {
+        pupusaqIngles.forEach((element, i) => {
             if(!element) return
              
-            document.querySelector("[data-pupusafq='"+i+"']").innerHTML = element;
+            document.querySelector("[data-pupusaq='"+i+"']").innerHTML = element;
         });
 
        sessionStorage.setItem("lang", "en");
@@ -118,9 +113,9 @@ function  traducir(element) {
                 document.querySelector("[data-nav='"+i+"']").innerHTML = element;
             }
         });
-        pupusafqEspañol.forEach((element, i) => {
+        pupusaqEspañol.forEach((element, i) => {
             if(!element) return
-            document.querySelector("[data-pupusafq='"+i+"']").innerHTML = element;
+            document.querySelector("[data-pupusaq='"+i+"']").innerHTML = element;
         });
 
        sessionStorage.setItem("lang", "es");
