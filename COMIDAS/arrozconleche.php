28ef71bc535806555214f4150aaf279d677133fb
Papapio123 <eduardopenate3@gmail.com>
2022-08-25 14:46:39 -0600

    QUESADILLA
    TRADUCCION

--- COMIDAS/arrozconleche.php
+++ COMIDAS/arrozconleche.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="../Css/translate.css">
     <link rel="stylesheet" href="../Css/menuD.css">
     <script defer src="../Js/menu.js"></script>
-    <script defer src="../Js/translate.js"></script>
+    <script defer src="../Js/translate_recetas-arroz-leche.js"></script>
     <title>Arroz con leche</title>
 </head>
 
@@ -23,15 +23,17 @@
         <div class="recetas">
             <img src="../Comida/arroz-con-leche.jpg" alt="">
             <div class="recetas-letras">
-                <h1>ARROZ CON LECHE</h1>
+                <h1><span data-arrozleche="1">ARROZ CON LECHE<span></h1>
                 <p>
+                <span data-arrozleche="2">
                     El arroz con leche es un postre casero y cremoso elaborado 
                     principalmente con arroz y leche, como su nombre indica. 
                     Su receta original es disfrutada en prácticamente cualquier lugar 
                     del mundo, lo que también ha dado lugar a algunas variedades sobre el 
                     postre tradicional. Aunque normalmente se consume de último en el menú y para
-                     cerrar la velada , en algunos países europeos se sirve como desayuno, ya que es 
+                     cerrar la velada, en algunos países europeos se sirve como desayuno, ya que es 
                      muy similar a las gachas de arroz.
+                     <span>
                 </p>
             </div>
         </div>
@@ -40,61 +42,61 @@
         </div>
         <div class="ingredientes">
             <div class="ingredientes2">
-                <h1>Ingredientes:</h1>
+                <h1><span data-arrozleche="3">Ingredientes:<span></h1>
             <p>
                 <ul type="square">
-                    <li> 4 tazas de leche</li>
-            <li> 6 tazas de agua</li>
-            <li> 1 taza de azúcar</li>
-            <li> 1/3 taza de arroz de grano largo</li>
-            <li> 4 canela en rama</li>
-            <li> 2 cucharadas de mantequilla</li>
-            <li> 1 pizca de sal</li>
-            <li> 1/8 cucharadita de nuez moscada en polvo</li>
-            <li> ¼ taza de pasas</li>
+                    <li><span data-arrozleche="4"> 4 tazas de leche<span></li>
+            <li><span data-arrozleche="5"> 6 tazas de agua<span></li>
+            <li><span data-arrozleche="6"> 1 taza de azúcar<span></li>
+            <li> <span data-arrozleche="7">1/3 taza de arroz de grano largo<span></li>
+            <li><span data-arrozleche="8"> 4 canela en rama<span></li>
+            <li><span data-arrozleche="9"> 2 cucharadas de mantequilla<span></li>
+            <li><span data-arrozleche="10"> 1 pizca de sal<span></li>
+            <li><span data-arrozleche="11"> 1/8 cucharadita de nuez moscada en polvo<span></li>
+            <li><span data-arrozleche="12"> ¼ taza de pasas<span></li>
                 </ul>
             </p>
             </div>
             <div class="ingredientes-pasos">
-                <h1>Preparación:</h1>
+                <h1><span data-arrozleche="13">Preparación:<span></h1>
                 <ol>
-                    <li>Preparación previa: Empapa el arroz en una mezcla de 1 taza de leche y 1 de agua y pon el refrigerador por unas horas antes.
-
+                    <li><span data-arrozleche="14">Preparación previa: Empapa el arroz en una mezcla de 1 taza de leche y 1 de agua y pon el refrigerador por unas horas antes.
+                    <span>
                     </li><br>
     
-                <li>Esto te ahorrará tiempo de cocción.
-
+                <li><span data-arrozleche="15">Esto te ahorrará tiempo de cocción.
+                <span>
                 </li><br>
     
-                <li>Pon las 4 tazas restantes de agua en un caldero y pon al fuego, agrega la canela y la sal.
-
+                <li><span data-arrozleche="16">Pon las 4 tazas restantes de agua en un caldero y pon al fuego, agrega la canela y la sal.
+                <span>
                 </li><br>
     
-                <li>Lleva ebullición y después agrega el arroz (con el líquido en que estaba remojado).
-
+                <li><span data-arrozleche="17">Lleva ebullición y después agrega el arroz (con el líquido en que estaba remojado).
+                <span>
                 </li><br>
 
-                <li>Revuelve regularmente para evitar que se adhiera al fondo.
-
+                <li><span data-arrozleche="18">Revuelve regularmente para evitar que se adhiera al fondo.
+                <span>
                 </li><br>
 
-                <li>Ajusta el agua cuando sea necesario y cuando el arroz esté muy blando agrega la leche y la mitad del azúcar.
-
+                <li><span data-arrozleche="19">Ajusta el agua cuando sea necesario y cuando el arroz esté muy blando agrega la leche y la mitad del azúcar.
+                <span>
                 </li><br>
 
-                <li>Cuando empiece a hervir agrega las pasas y el azúcar restante.
-
+                <li><span data-arrozleche="20">Cuando empiece a hervir agrega las pasas y el azúcar restante.
+                <span>
                 </li><br>
 
-                <li>Cuando tenga una consistencia muy espesa, casi sólida, retira del fuego y vierte inmediatamente en tazones pequeños o copas para helado.
-
+                <li><span data-arrozleche="21">Cuando tenga una consistencia muy espesa, casi sólida, retira del fuego y vierte inmediatamente en tazones pequeños o copas para helado.
+                <span>
                 </li><br>
 
-                <li>Espolvorea con nuez moscada moscada molida y deja refrescar.
-
+                <li><span data-arrozleche="22">Espolvorea con nuez moscada moscada molida y deja refrescar.
+                <span>
                 </li><br>
 
-                <li>Pon en el refrigerador por 30 minutos antes de servir.</li>
+                <li><span data-arrozleche="23">Pon en el refrigerador por 30 minutos antes de servir.<span></li>
             </ol>        
             </div>
         </div>
@@ -103,7 +105,7 @@
       <footer>
         <div class="contar">
             <h3 class="log">Los Tipiquisimos</h3>
-            <p>&copy; Los Tipiquisimos 2022-Derechos reservados</p>
+            <p>&copy;<span data-arrozleche="24"> Los Tipiquisimos 2022-Derechos reservados<span></p>
         </div>
         <div class="container3">
             <a href="https://www.facebook.com/Los-Tipiquisimos-110215061699961/?ref=page_internal"><img
---
+++ Js/translate_recetas-arroz-leche.js
@@ -0,0 +1,149 @@
+// let español, english;
+let arrozlecheEspañol = `
+ARROZ CON LECHE
+El arroz con leche es un postre casero y cremoso elaborado principalmente con arroz y leche, como su nombre indica. Su receta original es disfrutada en prácticamente cualquier lugar del mundo, lo que también ha dado lugar a algunas variedades sobre el postre tradicional. Aunque normalmente se consume de último en el menú y para cerrar la velada, en algunos países europeos se sirve como desayuno, ya que es muy similar a las gachas de arroz.
+Ingredientes:
+4 tazas de leche
+6 tazas de agua
+1 taza de azúcar
+1/3 taza de arroz de grano largo
+4 canela en rama
+2 cucharadas de mantequilla
+1 pizca de sal
+1/8 cucharadita de nuez moscada en polvo
+¼ taza de pasas
+Preparación:
+Preparación previa: Empapa el arroz en una mezcla de 1 taza de leche y 1 de agua y pon el refrigerador por unas horas antes.
+Esto te ahorrará tiempo de cocción.
+Pon las 4 tazas restantes de agua en un caldero y pon al fuego, agrega la canela y la sal.
+Lleva ebullición y después agrega el arroz (con el líquido en que estaba remojado).
+Revuelve regularmente para evitar que se adhiera al fondo.
+Ajusta el agua cuando sea necesario y cuando el arroz esté muy blando agrega la leche y la mitad del azúcar.
+Cuando empiece a hervir agrega las pasas y el azúcar restante.
+Cuando tenga una consistencia muy espesa, casi sólida, retira del fuego y vierte inmediatamente en tazones pequeños o copas para helado.
+Espolvorea con nuez moscada moscada molida y deja refrescar.
+Pon en el refrigerador por 30 minutos antes de servir.
+Los Tipiquisimos 2022-Derechos reservados
+`
+let arrozlecheIngles = `
+RICE PUDDING
+Rice pudding is a creamy homemade dessert made mainly with rice and milk, as its name suggests. Its original recipe is enjoyed almost everywhere in the world, which has also given rise to some varieties of the traditional dessert. Although it is usually consumed last on the menu and to close the evening, in some European countries it is served as breakfast, as it is very similar to rice porridge.
+Ingredients:
+4 cups of milk
+6 cups of water
+1 cup sugar
+1/3 cup long grain rice
+4 cinnamon sticks
+2 tablespoons butter
+1 pinch of salt
+1/8 teaspoon powdered nutmeg
+¼ cup raisins
+Preparation:
+Advance preparation: soak the rice in a mixture of 1 cup milk and 1 cup water and put in the refrigerator for a few hours beforehand.
+This will save you cooking time.
+Put the remaining 4 cups of water in a cauldron and heat, add the cinnamon and salt.
+Bring to a boil and then add the rice (with the liquid in which it was soaked).
+Stir regularly to prevent it from sticking to the bottom.
+Adjust the water when necessary and when the rice is very soft add the milk and half of the sugar.
+When it starts to boil add the raisins and the remaining sugar.
+When it has a very thick, almost solid consistency, remove from the heat and immediately pour into small bowls or ice cream cups.
+Sprinkle with ground nutmeg and let cool.
+Place in the refrigerator for 30 minutes before serving.
+Los Tipiquisimos 2022-Rights Reserved
+`
+let navEspañol = `
+Elegir idioma
+España
+Ingles
+¡Sobre nosotros!
+Recetas
+Navideñas
+Semana santa
+pupusas
+postres
+Tamales
+Sopas
+Recetas tipicas
+Atoles
+¿Quieres subir tu receta?
+Comunidad
+Inciar sesion
+`
+let navIngles = `
+Choose a language
+Spanish
+English
+¡About us!
+Recipes
+Christmas
+Easter
+pupusas
+desserts
+Tamales
+Soups
+Typical recipes
+Atoles
+¿Want to upload your recipes?
+Community
+Log in
+`
+arrozlecheIngles = arrozlecheIngles.split("\n");
+arrozlecheEspañol = arrozlecheEspañol.split("\n");
+
+navIngles = navIngles.split("\n");
+navEspañol = navEspañol.split("\n");
+
+lang = sessionStorage.getItem("lang");
+console.log(lang)
+
+if(!lang){
+    lang = sessionStorage.setItem("lang", "es");
+}
+
+
+
+function  traducir(element) {
+    console.log(element)
+    if(element.id == "idioma-ingles"){   
+        
+        navIngles.forEach((element, i) => {
+            if(!element) return
+             
+            if (document.querySelector("[data-nav='"+i+"']")) {
+                document.querySelector("[data-nav='"+i+"']").innerHTML = element;
+            }
+        });
+        arrozlecheIngles.forEach((element, i) => {
+            if(!element) return
+             
+            document.querySelector("[data-arrozleche='"+i+"']").innerHTML = element;
+        });
+
+       sessionStorage.setItem("lang", "en");
+       lang = sessionStorage.getItem("lang");
+
+
+    }
+    else if(element.id == "idioma-español"){
+        navEspañol.forEach((element, i) => {
+            if(!element) return
+            if (document.querySelector("[data-nav='"+i+"']")) {
+                
+                document.querySelector("[data-nav='"+i+"']").innerHTML = element;
+            }
+        });
+        arrozlecheEspañol.forEach((element, i) => {
+            if(!element) return
+            document.querySelector("[data-arrozleche='"+i+"']").innerHTML = element;
+        });
+
+       sessionStorage.setItem("lang", "es");
+       lang = sessionStorage.getItem("lang");
+    }
+    console.log(lang)
+}
+
+document.querySelector(".wrapper-caja").addEventListener("click", ()=>{
+    document.querySelector(".options").classList.toggle("mostrar");
+    document.querySelector(".caja__arrow").classList.toggle("changeOrientation");
+})
